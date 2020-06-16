1955c9aa72a183998151b7785a7b40994301f9db
Aliaksandr Padhainy <sxurik@gmail.com>
2020-06-16 13:46:46 +0300

    feat: add signin and signup logic

--- src/js/blocks/Authorization/authorization.js
+++ src/js/blocks/Authorization/authorization.js
@@ -2,5 +2,9 @@ import '../../../sass/styles.scss';
 
 import 'bootstrap/js/dist/tab';
 
+import Authorization from '../../modules/Authorization';
+
 window.onload = () => {
+  const authorization = new Authorization();
+  authorization.setEventListeners();
 };
