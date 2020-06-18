cf1135bed5593cfa07f6f32fe873a0d916dd375d
Anna234365 <6054404@gmail.com>
2020-06-18 15:20:37 +0300

    init: create scss file, add basic murkup and logic

--- src/js/blocks/Savannah/savannah.js
+++ src/js/blocks/Savannah/savannah.js
@@ -1,4 +1,18 @@
 import '../../../sass/styles.scss';
+import { savannahState } from './appState';
 
-window.onload = () => {
+import {
+  defineActiveRound, getWordsCollection, setWordsOrder, combineWordsAndAnswers,
+} from './getActiveWord';
+
+window.onload = async function onload() {
+  const [level, page] = defineActiveRound();
+  savannahState.lastPlayedRound = [level, page].join('.');
+  savannahState.currentLevel = level;
+  savannahState.currentPage = page;
+  savannahState.wordsCollection = await getWordsCollection();
+  savannahState.wordsOrder = setWordsOrder();
+  savannahState.wordAndAnswers.length = 0;
+  savannahState.wordAndAnswers = combineWordsAndAnswers();
+  // console.log(savannahState.wordAndAnswers);
 };
