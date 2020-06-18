2faf0843f5f6dcbcac69cd54e6f281032d552bbc
Anna234365 <6054404@gmail.com>
2020-06-18 15:20:37 +0300

    init: create scss file, add basic murkup and logic

---
+++ src/js/blocks/Savannah/appState.js
@@ -0,0 +1,10 @@
+export const savannahState = {
+  lastPlayedRound: '0.0',
+  currentLevel: '',
+  currentPage: '',
+  wordsCollection: [],
+  wordsOrder: [],
+  wordAndAnswers: [],
+};
+
+export const QUESTION = 'WhatShoulIDoWithExprtsOfConsts';
