1e2b63b246ae98c635cf78bf3fdb6492fd0467b1
Bekhruz Mirrakhimov <bexruz@gmail.com>
2020-06-22 19:34:43 +0500

    chore: rebasing, resolving conflicts

---
+++ src/js/blocks/Sprint/sprintStat.js
@@ -0,0 +1,30 @@
+// eslint-disable-next-line no-unused-vars
+import { divide } from 'lodash';
+import '../../../sass/styles.scss';
+
+const simpleModel = document.getElementById('simpleModal');
+const closeBtn = document.getElementsByClassName('closeBtn')[0];
+
+const openModal = () => {
+  simpleModel.style.display = 'block';
+};
+export { openModal as default };
+
+const closeModal = () => {
+  closeBtn.addEventListener('click', () => {
+    simpleModel.style.display = 'none';
+  });
+};
+
+const outsideClick = () => {
+  window.addEventListener('click', (e) => {
+    if (e.target === simpleModel) {
+      simpleModel.style.display = 'none';
+    }
+  });
+};
+
+window.addEventListener('load', () => {
+  closeModal();
+  outsideClick();
+});
