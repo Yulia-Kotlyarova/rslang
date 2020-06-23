68dfb372e0d1ce477c37f784fb5fb2103eca794c
Aliaksandr Padhainy <sxurik@gmail.com>
2020-06-23 15:00:56 +0300

    feat: add markup and styles for settings modal

--- src/js/blocks/Index/index.js
+++ src/js/blocks/Index/index.js
@@ -1,5 +1,10 @@
 import '../../../sass/styles.scss';
 import '@fortawesome/fontawesome-free/js/all.min';
 
+import 'bootstrap/js/dist/index';
+import 'bootstrap/js/dist/util';
+import 'bootstrap/js/dist/button';
+import 'bootstrap/js/dist/modal';
+
 window.onload = () => {
 };
