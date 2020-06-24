1063f95d23b3e55cd6d42e23389e700b3aca7a8c
Bekhruz Mirrakhimov <bexruz@gmail.com>
2020-06-24 16:41:47 +0500

    add: game statistics

--- src/js/blocks/Sprint/sprintStat.js
+++ src/js/blocks/Sprint/sprintStat.js
@@ -1,15 +1,37 @@
 // eslint-disable-next-line no-unused-vars
 import { divide } from 'lodash';
 import '../../../sass/styles.scss';
+import { objForCorrectWord, objForInCorrectWord } from './sprintGame';
 
 const simpleModel = document.getElementById('simpleModal');
 const closeBtn = document.getElementsByClassName('closeBtn')[0];
+// const incorrectWords = [];
+
+function disableKey() {
+  document.addEventListener('keydown', (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+    e.returnValue = false;
+    e.cancelBubble = true;
+    return false;
+  }, false);
+}
+
+const addWordsToStatistic = (array1, array2) => {
+  const list = document.createElement('ul');
+  for (let i = 0; i < array1.length; i += 1) {
+    const item = document.createElement('li');
+    item.appendChild(document.createTextNode(`${array1[i]} - ${array2[i]}`));
+    list.appendChild(item);
+  }
+  return list;
+};
 
 const openModal = () => {
   simpleModel.style.display = 'block';
-  document.onkeydown = (e) => {
-    e.preventDefault();
-  };
+  disableKey();
+  document.querySelector('.modal-content__body-correct').appendChild(addWordsToStatistic(objForCorrectWord.eng, objForCorrectWord.ru));
+  document.querySelector('.modal-content__body-error').appendChild(addWordsToStatistic(objForInCorrectWord.eng, objForInCorrectWord.ru));
 };
 
 export { openModal as default };
