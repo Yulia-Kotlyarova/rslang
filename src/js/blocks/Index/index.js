ebc05f99f0fca1879e41b3f042c8b43eaf4fc5c5
Aliaksandr Padhainy <sxurik@gmail.com>
2020-06-25 22:57:51 +0300

    feat: form validation

--- src/js/blocks/Index/index.js
+++ src/js/blocks/Index/index.js
@@ -1,4 +1,5 @@
 import '../../../sass/styles.scss';
+
 import '@fortawesome/fontawesome-free/js/all.min';
 
 import 'bootstrap/js/dist/index';
@@ -6,5 +7,10 @@ import 'bootstrap/js/dist/util';
 import 'bootstrap/js/dist/button';
 import 'bootstrap/js/dist/modal';
 
+import SettingsModal from './modules/SettingsModal';
+
 window.onload = () => {
+  const settingsModal = new SettingsModal();
+
+  settingsModal.initiate();
 };
