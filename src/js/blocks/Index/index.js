19180a1ccf795f625434dd1084c67a404caf2fb1
Aliaksandr Padhainy <sxurik@gmail.com>
2020-06-25 22:57:51 +0300

    feat: form validation

--- src/js/blocks/Index/index.js
+++ src/js/blocks/Index/index.js
@@ -288,7 +288,12 @@ class Card {
   }
 }
 
+<<<<<<< HEAD
 const card = new Card();
 card.showCard();
 card.getWord();
 card.setEventListener();
+=======
+  settingsModal.initiate();
+};
+>>>>>>> 5c395d2... feat: form validation
