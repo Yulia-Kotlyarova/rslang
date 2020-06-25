30acee3f802e43bb2bdf1d4d6f1a64afb2b14f7b
Yulia-Kotlyarova <60572747+Yulia-Kotlyarova@users.noreply.github.com>
2020-06-25 18:26:51 +0300

    refactor: delete commented code

--- src/js/blocks/AudioCall/audioCall.js
+++ src/js/blocks/AudioCall/audioCall.js
@@ -6,17 +6,10 @@ import 'bootstrap/js/dist/tab';
 import '@fortawesome/fontawesome-free/js/all.min';
 
 const dontKnowBtn = document.querySelector('.a-c-dont-know');
-// const nextBtn = document.querySelector('.a-c-next');
-// const playAnother = document.querySelector('.a-c-another-game-btn');
-// const playAgainBtn = document.querySelector('.a-c-again-btn');
 const volumeUp = document.querySelector('#big-volume-up');
 
 const volumeUpIcon = document.querySelector('.volume-up > audio');
 const wordList = document.querySelectorAll('.word-list > li');
-// let photo = document.querySelector('.audio-call-photo');
-
-// const min;
-// const max;
 
 function randomInteger(min, max) {
   const rand = min + Math.random() * (max + 1 - min);
@@ -51,7 +44,6 @@ function getCard(taskWord) {
     .then((resp) => {
       for (let i = 0; i < 5; i++) {
         const rand = randomInteger(1, 19);
-        // console.log(rand);
         wordList[i].textContent = resp[rand].wordTranslate;
       }
     })
@@ -78,7 +70,6 @@ function soundClick(taskWord) {
   volumeUpIcon.src = taskWord.audio;
   volumeUpIcon.attributes.autoplay = 'autoplay';
   const sound = taskWord.audio;
-  // console.log(sound);
   document.querySelector('.fa-volume-up').addEventListener('click', () => {
     const audio = new Audio();
     audio.src = sound;
@@ -87,57 +78,16 @@ function soundClick(taskWord) {
   getCard(taskWord);
 }
 
-function getWords() { // change link * *
+function getWords() { 
   fetch('https://afternoon-falls-25894.herokuapp.com/words?page=3&group=5')
     .then((response) => response.json())
     .then((response) => {
       const rand = randomInteger(1, 19);
       const taskWord = response[rand];
-      // console.log(taskWord);
       return taskWord;
     })
-    // .then((response) => createWordList(response))
     .then((taskWord) => soundClick(taskWord))
-    // .then((response) => createCard(response))
-    // .then((taskWord) => getCard(taskWord))
     .catch(() => (console.log('err')));
 }
 getWords();
 
-/*
-function createWordList(response) {
-  class WordTrain {
-    constructor(word, translate, image, audio) {
-      this.word = word;
-      this.translate = translate;
-      this.image = image;
-      this.audio = audio;
-    }
-  }
-
-  function newWord(response) {
-    let rand = randomInteger(min, max);
-    let word = new WordTrain(`${response[rand].word}`, `${response[rand].wordTranslate}`,
-    `${response[rand].audio}`, `${response[rand].image}`);
-    // console.log(word);
-    return word;
-  }
-
-  let word1 = newWord(response);
-  let word2 = newWord(response);
-  let word3 = newWord(response);
-  let word4 = newWord(response);
-  let word5 = newWord(response);
-
-  console.log(word1);
-  return (word1, word2, word3, word4, word5);
-}
-*/
-
-// console.log(learnWord);
-
-/*
-window.onload = () => {
-  getWordsArr();
-};
-*/
