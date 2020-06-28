0978edd841002c4c08a88a47dc9dcc83517c3d75
Aliaksandr Padhainy <sxurik@gmail.com>
2020-06-28 17:02:06 +0300

    feat: improve error handling, show message module on error

--- src/js/blocks/Authorization/authorization.js
+++ src/js/blocks/Authorization/authorization.js
@@ -3,12 +3,18 @@ import '../../../sass/styles.scss';
 import 'bootstrap/js/dist/tab';
 
 import Authorization from '../../modules/Authorization';
+import MessageModal from '../../modules/MessageModal';
 
 window.onload = () => {
   if (Authorization.isSignedUp() && !Authorization.isTokenExpired()) {
     window.location.href = 'index.html';
   }
 
+  localStorage.clear();
+
   const authorization = new Authorization();
   authorization.setEventListeners();
+
+  const messageModal = new MessageModal();
+  messageModal.appendSelf('authorization___modal');
 };
