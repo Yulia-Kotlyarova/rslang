5120abccc1ed118e46d62166efdbb80de9b38cf3
Yulia Kotlyarova <yuliakykyshka@gmail.com>
2020-07-03 17:09:47 +0300

    fix: resolve conflicts

--- src/js/blocks/AudioCall/audioCall.js
+++ src/js/blocks/AudioCall/audioCall.js
@@ -1,8 +1,17 @@
 /* eslint-disable no-plusplus */
 
 import '../../../sass/styles.scss';
+import 'bootstrap/js/dist/collapse';
+import '@fortawesome/fontawesome-free/js/all.min';
+import Header from '../../modules/Header';
 
-import AudioCall from '../../modules/AudioCall';
+export {
+  showRightAnswer, right, wrong, wrongAnswer, getCard, sayWord,
+  getWords, gameResult, nextCard, background,
+  rightNumber, wrongNumber,
+} from '../../modules/AudioCall';
 
 window.onload = () => {
+  const header = new Header();
+  header.run();
 };
