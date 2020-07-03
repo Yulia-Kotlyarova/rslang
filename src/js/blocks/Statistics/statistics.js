3f32a4fd546076475ecb29ee0c1eabaddb846308
Anna234365 <6054404@gmail.com>
2020-07-03 15:39:44 +0300

    add: statistics buttons

--- src/js/blocks/Statistics/statistics.js
+++ src/js/blocks/Statistics/statistics.js
@@ -3,17 +3,18 @@ import 'bootstrap/js/dist/collapse';
 import '@fortawesome/fontawesome-free/js/all.min';
 import Header from '../../modules/Header';
 import Chart from './Chart';
+import Buttons from './Buttons';
 
 import ShortStatistics from './ShortStatistics';
 
 const chart = new Chart();
 const shortStatistics = new ShortStatistics();
+const buttons = new Buttons(shortStatistics);
 
 window.onload = () => {
   const header = new Header();
   header.run();
-  chart.setEventListeners();
   chart.renderUserChart();
-
-  shortStatistics.showModal();
+  chart.setEventListeners();
+  buttons.setEventListeners();
 };
