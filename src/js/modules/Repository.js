3642193f8b1fe46ebf6149706c3748d75335e6c9
Eduard Brukish <eduardbrukish@gmail.com>
2020-07-04 09:46:15 +0300

    feat: resolve version conflicts

--- src/js/modules/Repository.js
+++ src/js/modules/Repository.js
@@ -439,7 +439,11 @@ class Repository {
       optional: settings.optional,
     };
 
+<<<<<<< HEAD
     const url = `${backendOrigin}/users/${userId}/statistics`;
+=======
+    const url = `https://afternoon-falls-25894.herokuapp.com/users/${userId}/settings`;
+>>>>>>> 4acb2c5... feat: resolve version conflicts
 
     const rawResponse = await fetch(url, {
       method: 'PUT',
