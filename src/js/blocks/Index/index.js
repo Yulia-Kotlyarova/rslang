9f4aa27dd1aed1444cb5ec0e227b9a6e41ce99b0
Eduard Brukish <eduardbrukish@gmail.com>
2020-07-04 09:36:42 +0300

    feat: resolve version styles conflict

--- src/js/blocks/Index/index.js
+++ src/js/blocks/Index/index.js
@@ -3,6 +3,11 @@ import '../../../sass/styles.scss';
 import '@fortawesome/fontawesome-free/js/all.min';
 import Header from '../../modules/Header';
 
+import 'bootstrap/js/dist/index';
+import 'bootstrap/js/dist/util';
+import 'bootstrap/js/dist/button';
+import 'bootstrap/js/dist/modal';
+
 window.onload = () => {
   const header = new Header();
   header.run();
