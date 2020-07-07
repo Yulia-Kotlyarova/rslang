ff072b3a920572c373e9646ca9ea0e5c21d03ad5
Anna234365 <6054404@gmail.com>
2020-07-07 23:07:45 +0300

    add chenges

---
+++ src/js/blocks/Puzzle/UserWords.js
@@ -0,0 +1,12 @@
+export default class UserWords {
+  constructor(userWords) {
+    this.userWords = userWords;
+    this.userWordsLength = 0;
+  }
+
+  // getUserWords() {
+  //   this.userWords = this.userWords.filter((word) => word.textExample.split(' ').length < 11);
+  //   if (this.userWords.length < 10) {
+  //   }
+  // }
+}
